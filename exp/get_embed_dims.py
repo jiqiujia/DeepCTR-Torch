6c824f2d1f39b14c880dafa48ac01f85fbc51ba7
jiqiujia <qiujiaji@tencent.com>
2020-01-01 23:14:48 +0800

    optimize get_embeds_dim

--- exp/get_embed_dims.py
+++ exp/get_embed_dims.py
@@ -3,6 +3,7 @@ import os
 import sys
 import io
 import glob
+import pandas as pd
 
 if __name__ == '__main__':
     root = sys.argv[1]
@@ -12,14 +13,21 @@ if __name__ == '__main__':
     with io.open(header_file, encoding='utf-8') as fin:
         header = fin.readlines()[0].strip().split(',')
     data_paths = glob.glob(root)
+    print(data_paths)
 
     unique_vals = [0 for _ in range(len(header))]
     for path in data_paths:
-        with io.open(path, encoding='utf-8') as fin:
-            for line in fin:
-                arr = line.strip().split(',')
-                for i, val in enumerate(arr):
-                    unique_vals[i] = max(unique_vals[i], int(float(val)))
+        print(path)
+        df = pd.read_csv(path)
+        maxs = df.max()
+        for i, val in enumerate(maxs):
+            unique_vals[i] = max(unique_vals[i], int(float(val)))
+        # with io.open(path, encoding='utf-8') as fin:
+        #     for line in fin:
+        #         arr = line.strip().split(',')
+        #         for i, val in enumerate(arr):
+        #             unique_vals[i] = max(unique_vals[i], int(float(val)))
+
 
     header_dim_dict = {}
     for col, val in zip(header, unique_vals):
